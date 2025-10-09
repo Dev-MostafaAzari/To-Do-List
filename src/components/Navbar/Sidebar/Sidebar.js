ad79cc5f2475c2ea22bc25a3c50f998571102966
Mostafa <msari808755@gmail.com>
2025-10-09 03:27:15 -0700

    SidebarAnimationAndStyling

--- src/components/Navbar/Sidebar/Sidebar.js
+++ src/components/Navbar/Sidebar/Sidebar.js
@@ -1,25 +1,63 @@
 import React, { useState } from 'react';
-import {motion} from "framer-motion";
+import { animate, motion } from "framer-motion";
 import "../../../styles/Sidebar.css";
 import ToggleButton from '../ToggleButton/ToggleButton';
 
 
-const Sidebar = ()=> {
-    const [open,setOpen]=useState(false);
+const Sidebar = () => {
+    const [open, setOpen] = useState(false);
+
+    const variants = {   //variant baray bg
+        open: {
+
+            clipPath: "circle(1800px at 25px 25px)",
+            transition: {
+                duration: 1,
+
+            }
+        },
+        close:
+        {
+            clipPath: "circle(30px at 45px 40px)",
+            transition: {
+                duration: 0.5,
+            }
+        },
+    }
+
+    const ListVariants = {   //variant baray list ha
+        close: {
+            y: 100,
+            opacity:0,
+            transition: {
+                duration: 0.5,
+                staggerChildren: 0.1,
+            },
+        },
+        open: {
+            y: 0,
+            opacity:1,
+            transition: {
+                duration: 0.5,
+                staggerChildren: 0.1,
+            },
+        },
+
+    }
 
 
-
-    return(
+    return (
         <div className="Sidebar">
-            <div className="bg">
-                <div className="Links">
-                    <a href=''>Login</a>
-                    <a href=''>To Do List</a>
-                    <a href=''>Logout</a>
-                </div>
-                <ToggleButton setOpen={setOpen}/>
-            </div>
-            
+            <motion.div className="bg" variants={variants} animate={open ? "open" : "close"}>
+                <motion.div className="Links" variants={ListVariants} animate={open ? "open" : "close"}>
+                    <motion.a variants={ListVariants} whileHover={{scale:1.1,transition:{duration:0.3}}} whileTap={{scale:0.9}} href='/home'>Home Page</motion.a>
+                    <motion.a variants={ListVariants} whileHover={{scale:1.1,transition:{duration:0.3}}} whileTap={{scale:0.9}} href='/Login'>Login</motion.a>
+                    <motion.a variants={ListVariants} whileHover={{scale:1.1,transition:{duration:0.3}}} whileTap={{scale:0.9}} href='#'>To Do List</motion.a>
+                    <motion.a variants={ListVariants} whileHover={{scale:1.1,transition:{duration:0.3}}} whileTap={{scale:0.9}} href='#'>Logout</motion.a>
+                </motion.div>
+                <ToggleButton setOpen={setOpen} />
+            </motion.div>
+
         </div>
     )
 }
