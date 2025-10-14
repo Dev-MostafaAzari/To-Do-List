20201634ee03e6f9b33d31fe3d0fcefbdc66c6f2
Mostafa <msari808755@gmail.com>
2025-10-14 05:15:06 -0700

    TodoListAndAddTaskStyling

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {motion} from "framer-motion";
 import "../../styles/ToDoList.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClose, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 
 const DoList = ()=>{
@@ -11,12 +13,46 @@ const DoList = ()=>{
                     <h1>Your ToDo List</h1>
                 </div>
                 <div className="DoLists">
-                    <div className="ToDoItem">
-                        <p>Send hello to your friends</p>
-                        <div className="TodoButton">
-                            <button></button>
-                        </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>ToDo Item</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>send hello to your friends</td>
+                                <td><input type="checkbox"/></td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>send hello to your friends</td>
+                                <td><input type="checkbox"/></td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>send hello to your friends</td>
+                                <td><input type="checkbox"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="buttons">
+                    <button>Add Task</button>
+                </div>
+                <div className="AddTask">
+                    <div className="AddTaskCloseBtn">
+                        <button><FontAwesomeIcon icon={faClose}/></button>
                     </div>
+                    <h1>Add New Task</h1>
+                    <form className="AddTaskInput">
+                        <input type="text" placeholder="Task Name"/>
+                        <input type="number" placeholder="UserID"/>
+                        <button type="submit">Add Task</button>
+                    </form>
                 </div>
             </div>
         </div>
