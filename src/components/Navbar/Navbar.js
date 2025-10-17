3dd3ce4c7a0a50e33fcadce0dc2aabde93c4b17d
Mostafa <msari808755@gmail.com>
2025-10-17 08:12:30 -0700

    socialLinks

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {motion} from "framer-motion";
 import "../../styles/Navbar.css";
 import Sidebar from './Sidebar/Sidebar';
-import Facebook from "../../assets/facebook.png";
+import Github from "../../assets/github.png";
 import Instagram from "../../assets/instagram.png";
 
 
@@ -14,8 +14,8 @@ const Navbar =()=>{
             <div className="NavbarWrapper">
                 <h1>Dev Mostafa</h1>
                 <div className="Social">
-                    <a href='#'><img src={Facebook} alt="facebook"/></a>
-                    <a href='#'><img src={Instagram} alt="instagram"/></a>
+                    <a href="https://github.com/Dev-MostafaAzari" target="_blank"><img src={Github} alt="facebook"/></a>
+                    <a href="https://instagram.com/mostafazari224" target="_blank"><img src={Instagram} alt="instagram"/></a>
                 </div>
             </div>
             
