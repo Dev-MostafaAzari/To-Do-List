ba60f0e0dea5e954f89b7bb87cd2590f19d0e62e
Mostafa <msari808755@gmail.com>
2026-04-24 08:17:48 -0700

    AllTodosResponsive

--- src/components/ToDoList/AllTodos/AllTodos.js
+++ src/components/ToDoList/AllTodos/AllTodos.js
@@ -25,40 +25,43 @@ const AllTodos = () => {
                         <p className="EmptyTodosTitle2">Try Create New Task😉</p>
                     </div>    
                     :
-                    <div className="AllTodosList">
-                        {TodoList.map((item) => 
-                            <div className="AllTodoTaskCard">
-                                <div className="AllTodoTaskCardTitle">
-                                    <p className="AllTodoCardTitle">{item.Title}</p>
-                                </div>
-                                <div className="AllTodoTaskDescript">
-                                    <p className="AllTodoCardDiscript"><span>Discription :</span>{item.Descript} </p>
-                                    <div className="AllTodoOperationBtns">
-                                        <button className="CheckTodo" title="Complete" onClick={()=>{dispatch(TaskDone(item.id))}}><FontAwesomeIcon icon={faCheckSquare}/></button>
-                                        <button className="DeleteTodo" title="Delete" onClick={()=>{setDeleteAlret(prev=>prev=!prev);setDeleteAlertId(item.id)}}><FontAwesomeIcon icon={faTrashCan}/></button>
-                                    </div>    
-                                </div>
-                                <span className="AllTodoCardSpan" style={{background:(item.Priority === "High" ? "red" : (item.Priority === "Mid" ? "Yellow" : "Gray" ))}}>
-                                    {item.Priority}
-                                </span>
-                                <button className="ViewThisTask" onClick={()=>{dispatch(ViewTask(item.id))}} title="ViewTask"><FontAwesomeIcon icon={faEye}/></button>
-                            </div>
-                        )}
-                        {deleteAlert ? 
-                            <div className="DeleteAlertContainer">
-                                <div className="DeleteConfirmationContainer">
-                                    <p>Are you sure about deleting this task?😦</p>
-                                    <div>
-                                        <button id="CancelDelete" onClick={()=>{setDeleteAlret(prev=>prev=!prev)}}>Cancel</button>
-                                        <button id="ConfirmDelete" onClick={()=>{DeleteItem(deleteItemId);setDeleteAlret(prev=>prev=!prev)}}>Delete</button>
+                    <div className="AllTodosListMain">
+                        <div className="AllTodosList">
+                            {TodoList.map((item) => 
+                                <div className="AllTodoTaskCard">
+                                    <div className="AllTodoTaskCardTitle">
+                                        <p className="AllTodoCardTitle">{item.Title}</p>
                                     </div>
+                                    <div className="AllTodoTaskDescript">
+                                        <p className="AllTodoCardDiscript"><span>Discription :</span>{item.Descript} </p>
+                                        <div className="AllTodoOperationBtns">
+                                            <button className="CheckTodo" title="Complete" onClick={()=>{dispatch(TaskDone(item.id))}}><FontAwesomeIcon icon={faCheckSquare}/></button>
+                                            <button className="DeleteTodo" title="Delete" onClick={()=>{setDeleteAlret(prev=>prev=!prev);setDeleteAlertId(item.id)}}><FontAwesomeIcon icon={faTrashCan}/></button>
+                                        </div>    
+                                    </div>
+                                    <span className="AllTodoCardSpan" style={{background:(item.Priority === "High" ? "red" : (item.Priority === "Mid" ? "Yellow" : "Gray" ))}}>
+                                        {item.Priority}
+                                    </span>
+                                    <button className="ViewThisTask" onClick={()=>{dispatch(ViewTask(item.id))}} title="ViewTask"><FontAwesomeIcon icon={faEye}/></button>
                                 </div>
+                            )}
                             
-                            </div>
-                            : 
-                            null
-                        }
-                    </div>    
+                        </div>
+                        {deleteAlert ? 
+                                <div className="DeleteAlertContainer">
+                                    <div className="DeleteConfirmationContainer">
+                                        <p>Are you sure about deleting this task?😦</p>
+                                        <div>
+                                            <button id="CancelDelete" onClick={()=>{setDeleteAlret(prev=>prev=!prev)}}>Cancel</button>
+                                            <button id="ConfirmDelete" onClick={()=>{DeleteItem(deleteItemId);setDeleteAlret(prev=>prev=!prev)}}>Delete</button>
+                                        </div>
+                                    </div>
+                                
+                                </div>
+                                : 
+                                null
+                        }  
+                    </div>  
                 }
             </div>
         </div>
